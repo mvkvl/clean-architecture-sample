7a59175ba5830ddd73d8f91169ae70da32339b83
Mikhail Kantur <kami@slink.ws>
2023-10-22 21:50:59 +1000

    code cleanup

--- domain/domain-entity/src/main/java/ws/slink/ca/domain/exception/UncheckedException.java
+++ domain/domain-entity/src/main/java/ws/slink/ca/domain/exception/UncheckedException.java
@@ -2,6 +2,14 @@ package ws.slink.ca.domain.exception;
 
 public class UncheckedException extends RuntimeException {
 
+    public UncheckedException() {
+        super();
+    }
+
+    public UncheckedException(String message) {
+        super(message);
+    }
+
     public UncheckedException(Throwable cause) {
         super(cause);
     }
